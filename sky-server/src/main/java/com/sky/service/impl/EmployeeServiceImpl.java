0b358b9a5061718e90558888d6e884b5526c8a6d
Howay-Che <caihaowei328@gmail.com>
2024-09-24 21:55:12 +0800

    新增AOP功能

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -83,15 +83,6 @@ public class EmployeeServiceImpl implements EmployeeService {
         //设置账号密码
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
-        //设置创建时间和修改时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
-
-        //设置当前记录创建人id和修改人id
-        Long id = BaseContext.getCurrentId();
-        employee.setCreateUser(id);
-        employee.setUpdateUser(id);
-
         employeeMapper.insert(employee);
     }
 
@@ -116,10 +107,6 @@ public class EmployeeServiceImpl implements EmployeeService {
      * @param id
      */
     public void startOrStop(Integer status, Long id) {
-        //update employee set status = ? where id = ?
-//        Employee employee = new Employee();
-//        employee.setId(id);
-//        employee.setStatus(status);
         Employee employee = Employee.builder()
                 .id(id)
                 .status(status)
@@ -148,8 +135,6 @@ public class EmployeeServiceImpl implements EmployeeService {
     public void update(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
         employeeMapper.update(employee);
     }
 
